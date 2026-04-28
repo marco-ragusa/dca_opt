1a0a21c6923992811d1694b2bdfa8021acee627a
marco-ragusa <macroragusa@gmail.com>
2026-04-28 23:49:36 +0200

    Enable Tailwind darkMode class strategy

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{svelte,ts}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
