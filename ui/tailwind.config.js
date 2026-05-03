6c29e00734e93f7ad0ad4380d7a6863ad7764253
marco-ragusa <macroragusa@gmail.com>
2026-05-03 13:04:04 +0200

    Migrate Tailwind CSS from v3 to v4 via Vite plugin

--- ui/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{svelte,ts}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
